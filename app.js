33c4773f03cac6a0a1225411edf6ade70f0afdad
Tomisin <tomisinjoseph3@gmail.com>
2023-11-06 22:07:50 +0000

    Added additional conditions

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection.toLowerCase() === "rock" & computerSelection === "paper") {
         return "You Lose! Paper beats Rock";
-    }
+    } else if (playerSelection.toLowerCase() === "paper" & computerSelection === "paper") {
+        return "It's a draw"; 
+    } else if (playerSelection.toLowerCase() === "scissors" & computerSelection === "paper") {
+        return "You win! Scissors beats Paper"; 
+    } else if (playerSelection.toLowerCase() === "rock" & computerSelection === "rock") {
+        return "It's a draw"; 
+    } else if (playerSelection.toLowerCase() === "paper" & computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection.toLowerCase() === "scissors" & computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors"; 
+    } else if (playerSelection.toLowerCase() === "scissors" & computerSelection === "scissors") {
+        return "It's a draw";
+    } else if (playerSelection.toLowerCase() === "paper" & computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection.toLowerCase() === "rock" & computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    } 
 }
 
 const playerSelection = prompt("rock, paper or scissors");
---
+++ app.test.js
@@ -0,0 +1,5 @@
+// const app = require('./app.js')
+
+// test('function gives a result of', () => {
+//     expect()
+// });
